ba95d5808122f0ba8f16bfd59d54b755729201ea
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2022-01-07 00:01:29 -0500

    Remove go.mod requirement from test scripts

--- example/linux.sh
+++ example/linux.sh
@@ -1,1 +1,2 @@
-cd .. && go build -ldflags="-s -w" -o example/autogo && cd example && ./autogo
+export GO111MODULE=off
+cd .. && go build -ldflags="-s -w" -o example/autogo && cd example && ./autogo "$@"
--- example/windows.ps1
+++ example/windows.ps1
@@ -1,1 +1,2 @@
+$env:GO111MODULE = "off"
 cd .. && (go build -ldflags="-s -w" -o example\autogo.exe || (cd example && Write-Error 'Failed to build autogo.' -ErrorAction Stop)) && cd example && .\autogo.exe $args
