e9af01771066b4380397b69816e1395528304053
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2022-01-07 22:18:22 -0500

    Restore forgotten autogo cmd

---
+++ cmd/autogo/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/JoshuaDoes/autogo/autoit"
+)
+
+var (
+	Script = ";INTERNAL SCRIPT\n#include \"main.au3\""
+)
+
+func runVM(scriptPath string, script []byte) error {
+	vm, err := autoit.NewAutoItScriptVM(scriptPath, script, nil)
+	if err != nil {
+		return err
+	}
+
+	return vm.Run()
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		scripts := make([]string, 0)
+		for _, arg := range os.Args[1:] {
+			switch strings.ToLower(arg) {
+			case "/errorstdout":
+				os.Stderr = os.Stdout
+			default:
+				scripts = append(scripts, arg)
+			}
+		}
+		for _, scriptFile := range scripts {
+			script, err := os.ReadFile(scriptFile)
+			if err != nil {
+				panic(err)
+			}
+
+			err = runVM(scriptFile, script)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	} else {
+		err := runVM("SCRIPT.AU3", []byte(Script))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
